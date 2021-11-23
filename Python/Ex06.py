525272e18ae01bc2e38b404601755555f3db3304
wilsontadashisakaragui <89815739+wilsontadashisakaragui@users.noreply.github.com>
2021-11-22 22:05:20 -0300

    Delete Ex06.py

--- Python/Ex06.py
+++
@@ -1,8 +0,0 @@
-# Conversão de quantia de dólares em reais
-
-v = float(input('Digite a cotação do dólar: '))
-x = float(input('Digite a quantia de dólares: '))
-
-reais = v * x
-
-print(f"O valor em reais é: {reais:.2f}")
