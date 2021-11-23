ac4e15b2afd573cfebb36667cf7dcf9f8b67bd6e
wilsontadashisakaragui <89815739+wilsontadashisakaragui@users.noreply.github.com>
2021-11-22 22:06:45 -0300

    Delete Ex01.py

--- Python/Ex01.py
+++
@@ -1,9 +0,0 @@
-#Calcular a área do retângulo
-
-base = int(input('Digite a base do retângulo: '))
-
-altura = int(input('Digite a altura do retângulo: '))
-
-area = base * altura
-
-print(f"A área do retângulo é: {area:}")
