adab615c43e67580bdce224cf4071abbf16b2f7b
wilsontadashisakaragui <89815739+wilsontadashisakaragui@users.noreply.github.com>
2021-11-22 22:06:18 -0300

    Delete Ex03.py

--- Python/Ex03.py
+++
@@ -1,9 +0,0 @@
-#Calcular a área do triângulo
-
-base = float(input('Digite a base do triângulo: '))
-
-altura = float(input('Digite a altura do triângulo: '))
-
-area = (base*altura)/2
-
-print(f"A área do retângulo é: {area:.2f}")
